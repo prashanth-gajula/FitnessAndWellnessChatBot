d07ed5431cbc8d03b57c22a07c111a9abc9493b5
Prashanth <kumarprashanth782@gmail.com>
2025-08-08 16:45:08 -0500

    setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FitnessAndWellnessChatBot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Prashanth",
+    author_email="kumarprashanth782@gmail.com"
+)
